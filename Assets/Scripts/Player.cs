42772da4aa2942e9fb275873f79beab336138c11
Michael Nguyen <michaelnguyenm@gmail.com>
2018-01-27 12:23:40 -0600

    Removed stuff and reimplemented players

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPlayer {
+    float speedX { get; set; }
+    float speedY { get; set; }
+
+    // Facing direction
+
+    // Move left/right
+
+    // Move up/down
+
+    // Magnet or weapon
+
+    // Fire
+}
---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour, IPlayer {
+
+    float IPlayer.speedX { get; set; }
+    float IPlayer.speedY { get; set; }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
