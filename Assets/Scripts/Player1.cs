975270835fc40a77d24e5037bea430248aa0b83e
sushibend <cheeeyman78@gmail.com>
2018-01-27 13:43:34 -0600

    fjajfioaioahgohghowehiohgozhoihgioh

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -6,7 +6,13 @@ public class Player1 : MonoBehaviour, IPlayer {
 
     float IPlayer.speedX { get; set; }
     float IPlayer.speedY { get; set; }
+	const float SPEED = 0.1f;
 
+	Vector3 mouse_pos;
+	Transform target;
+	Vector3 object_pos;
+	float angle;
+	public GameObject mouse;
     // Use this for initialization
     void Start () {
 		
@@ -14,6 +20,33 @@ public class Player1 : MonoBehaviour, IPlayer {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 pos = this.transform.position;
+		if (Input.GetKey (KeyCode.A))
+			pos.x -= SPEED;
+		if (Input.GetKey (KeyCode.D))
+			pos.x += SPEED;
+		if (Input.GetKey (KeyCode.S))
+			pos.y -= SPEED;
+		if (Input.GetKey (KeyCode.W))
+			pos.y += SPEED;
+		this.transform.position = pos;
+
+		/*
+		mouse_pos = Input.mousePosition;
+		mouse_pos.x = mouse_pos.x - pos.x;
+		mouse_pos.y = mouse_pos.y - pos.y;
+		angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+		Quaternion q = Quaternion.Euler(0f,0f,angle);
+		transform.rotation = q;
+	
+
+		float AngleRad = Mathf.Atan2 (mouse.transform.position.y - this.transform.position.y, mouse.transform.position.x - this.transform.position.x);
+		float AngleDeg = (180 / Mathf.PI) * AngleRad;
+		this.transform.rotation = Quaternion.Euler (0, 0, AngleDeg);
+		*/
+
+		Vector3 LookPos = new Vector3 (0, 0, transform.position.z);
+		gameObject.transform.LookAt (LookPos);
+	
 	}
 }
